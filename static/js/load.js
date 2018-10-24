cc4b94c07d43d4660ebc6092faea2d1de4e2be5b
Josh Lopes <jmlopes1283@gmail.com>
2018-10-23 22:49:36 -0400

    css fix

--- static/js/load.js
+++ static/js/load.js
@@ -13,12 +13,7 @@ function scrollFunc() {
 }
 
 function loadFunc() {
-	document.getElementById("rocket").classList.add("boom");
-	document.getElementById("smoke").classList.add("boom");
-	document.getElementById("main").classList.add("active");
-}
-
-function resizeFunc() {
+	document.getElementById("main").classList.remove("active");
 	document.getElementById("rocket").classList.remove("boom");
 	document.getElementById("smoke").classList.remove("boom");
 	document.getElementById("rocket").classList.add("hide");
@@ -28,7 +23,25 @@ function resizeFunc() {
 		document.getElementById("smoke").classList.remove("hide");
 	}, 500);
 	setTimeout(function(){ 
+		document.getElementById("main").classList.add("active");
 		document.getElementById("rocket").classList.add("boom");
 		document.getElementById("smoke").classList.add("boom");
 	}, 1000);
+}
+
+function resizeFunc() {
+	if (window.innerWidth >= 550 && window.matchMedia("(orientation: landscape)").matches) {
+		document.getElementById("rocket").classList.remove("boom");
+		document.getElementById("smoke").classList.remove("boom");
+		document.getElementById("rocket").classList.add("hide");
+		document.getElementById("smoke").classList.add("hide");
+		setTimeout(function(){ 
+			document.getElementById("rocket").classList.remove("hide");
+			document.getElementById("smoke").classList.remove("hide");
+		}, 500);
+		setTimeout(function(){ 
+			document.getElementById("rocket").classList.add("boom");
+			document.getElementById("smoke").classList.add("boom");
+		}, 1000);
+	}
 }
